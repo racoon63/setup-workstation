b91c809be1c6c37cc6820925b0a89a1d98a856a1
racoon63 <racoon63@gmx.net>
2019-05-03 15:16:40 +0200

    added vs-code server + firefox auto open

--- create-workstation.sh
+++ create-workstation.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-docker rmi $(docker images | grep "^<none>" | awk '{print $3}')
-docker build -t racoon/workstation . 2>&1 | tee build.log
-docker run -it --rm --name=workstation -v ~/.ssh:/root/.ssh racoon/workstation:latest
+docker rmi $(docker images | grep "^<none>" | awk '{print $3}')                                                                 # removes failed build images
+docker build -t racoon/workstation . 2>&1 | tee build.log                                                                       # builds container with needed tools in it
+docker run -d -p 127.0.0.1:80:8443 --rm --name=vs-code -v "${HOME}/github:/github" codercom/code-server --allow-http --no-auth    # starts a web based visual studio code server locally 
+firefox "localhost" &
+docker run -it --rm --name=workstation -v ~/.ssh:/root/.ssh racoon/workstation:latest                                           # starts built container
+
+read -p "Do you want to terminate the vs-code server too? [Y/n]: " VS_TERMINATION
+
+if [[ $VS_TERMINATION == 'Y' || $VS_TERMINATION == 'y' || $VS_TERMINATION == '' ]]; then
+    docker stop $(docker ps | grep "vs-code" | awk '{print $1}') 2>&1 > /dev/null
+fi
