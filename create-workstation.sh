506323d63e5c7a1d16e8305bbfdf0eb32045e61b
racoon63 <racoon63@gmx.net>
2019-05-03 14:24:06 +0200

    initial script to create container and start it immediatelly

--- create-workstation.sh
+++ create-workstation.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-docker build -t racoon/workstation --build-arg ssh_prv_key="$(cat ~/.ssh/id_ed25519)" --build-arg ssh_pub_key="$(cat ~/.ssh/id_ed25519.pub)" .
+docker rmi $(docker images | grep "^<none>" | awk '{print $3}')
+docker build -t racoon/workstation . 2>&1 | tee build.log
+docker run -it --rm --name=workstation -v ~/.ssh:/root/.ssh racoon/workstation:latest
