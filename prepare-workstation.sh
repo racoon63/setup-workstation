c815e19cac73a56db7eb99aca3568e4f9a9488c2
racoon63 <racoon63@gmx.net>
2019-05-03 14:24:25 +0200

    script to customize container environment

---
+++ prepare-workstation.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "### WORKSTATION SETUP ###"
+
+echo "### Starting ssh-agent ..."
+eval $(ssh-agent)
+
+echo "### Adding ssh key to ssh-agent inventory ..."
+ssh-add ~/.ssh/id_ed25519
+
+echo "### Cloning needed  ..."
+cd /github
+git clone git@github.com:racoon63/kubernetes.git
+git clone git@github.com:racoon63/setup-workstation.git
+cd ~
+
+echo "### Finished! Workstation is ready!"
+bash
