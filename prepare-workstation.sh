785f53d6be79f6fbf37dc45c54fdde7483715898
racoon63 <racoon63@gmx.net>
2019-05-06 14:27:13 +0200

    edited comments for claification

--- prepare-workstation.sh
+++ prepare-workstation.sh
@@ -8,11 +8,11 @@ eval $(ssh-agent)
 echo "### Adding ssh key to ssh-agent inventory ..."
 ssh-add ~/.ssh/id_ed25519
 
-echo "### Cloning needed  ..."
+echo "### Cloning repositories  ..."
 cd /github
 git clone git@github.com:racoon63/kubernetes.git
 git clone git@github.com:racoon63/setup-workstation.git
 cd ~
 
-echo "### Finished! Workstation is ready!"
+echo "### Finished! Dropping you into shell ..."
 bash
