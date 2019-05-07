dcc6e792b53597e05615e71a86f2618efbd13525
racoon63 <racoon63@gmx.net>
2019-05-07 15:34:04 +0200

    global git seetings

--- prepare-workstation.sh
+++ prepare-workstation.sh
@@ -8,6 +8,10 @@ eval $(ssh-agent)
 echo "### Adding ssh key to ssh-agent inventory ..."
 ssh-add ~/.ssh/id_ed25519
 
+echo "### Configure git settings ..."
+git config --global user.name "Patrick Lubach"
+git config --global user.email "p.lubach@goreply.de"
+
 echo "### Cloning repositories  ..."
 cd /github
 git clone git@github.com:racoon63/kubernetes.git
