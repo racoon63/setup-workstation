ad71c4cc971d6c06e0d9bb26fbc746f413bde048
racoon63 <racoon63@gmx.net>
2020-10-25 19:57:16 +0100

    minor update

---
+++ ubuntu/README.md
@@ -0,0 +1,8 @@
+# Setup Ubuntu Desktop Workstation
+
+## Useful links
+
+* [zsh](https://github.com/ohmyzsh/ohmyzsh/wiki/Installing-ZSH)
+* [oh-my-zsh](https://github.com/ohmyzsh/ohmyzsh)
+* [powerline fonts](https://github.com/powerline/fonts)
+* [vscode](https://github.com/racoon63/vscode)
--- ubuntu/install-ansible.sh
+++ ubuntu/install-ansible.sh
@@ -3,7 +3,7 @@
 set -e
 
 sudo apt update -y
-sudo apt dist-upgrade -y
+sudo apt upgrade -y
 sudo apt autoremove -y
 
 sudo apt install -y python3 python3-pip
@@ -11,3 +11,4 @@ sudo apt install -y python3 python3-pip
 pip3 install --user ansible
 
 echo "PATH=$PATH:$HOME/.local/bin" >> $HOME/.bashrc
+source $HOME/.bashrc
